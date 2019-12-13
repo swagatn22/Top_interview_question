5df8666011106525fab61755738269b861eea10e
swagatn22 <55399606+swagatn22@users.noreply.github.com>
2019-12-13 14:37:26 +0530

    Create Rmv_Dupl_frm_Srtd_array.cpp

---
+++ Rmv_Dupl_frm_Srtd_array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+        if(nums.size() == 0) return 0;
+        if(nums.size() == 1) return nums[0];
+        for(int i = 1;i<nums.size();i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+                
+            }
+        }
+        return j+1; 
+        
+        
+    }
+};
