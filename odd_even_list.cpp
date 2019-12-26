1fa1e0845696c3d745b75e575ec040dce74eefea
swagatn22 <55399606+swagatn22@users.noreply.github.com>
2019-12-26 14:22:45 +0530

    Create odd_even_list.cpp

---
+++ odd_even_list.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL) return NULL;
+        ListNode* oddH = head;
+        ListNode* evenH = head->next;
+        ListNode* even = evenH;
+        ListNode* odd = oddH; 
+        while(even != NULL && even->next != NULL){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenH;
+        return head;
+    }
+};
